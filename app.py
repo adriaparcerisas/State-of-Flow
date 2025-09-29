79ee5401bf4f7721e74eebfdac504522ab7602b4
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2025-09-29 21:32:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -367,34 +367,21 @@ ORDER BY period DESC;
 # ---------- SQL: Active Accounts ----------
 SQL_ACTIVE_ACCOUNTS_TOTAL = """
 WITH time_periods AS (
-  SELECT
-    CASE '{{Period}}'
-      WHEN 'all_time'      THEN '2020-01-01'::DATE
-      WHEN 'last_year'     THEN CURRENT_DATE - INTERVAL '1 YEAR'
-      WHEN 'last_3_months' THEN CURRENT_DATE - INTERVAL '3 MONTHS'
-      WHEN 'last_month'    THEN CURRENT_DATE - INTERVAL '1 MONTH'
-      WHEN 'last_week'     THEN CURRENT_DATE - INTERVAL '1 WEEK'
-      ELSE CURRENT_DATE - INTERVAL '1 DAY'
-    END AS start_date
-),
-active_addresses AS (
-  -- Cadence
-  SELECT COUNT(DISTINCT CAST(a.value AS VARCHAR)) AS active_addresses
-  FROM flow.core.ez_transaction_actors b,
-       LATERAL FLATTEN(INPUT => b.actors) a
-  CROSS JOIN time_periods tp
-  WHERE b.block_timestamp >= tp.start_date
-
-  UNION ALL
-
-  -- EVM
-  SELECT COUNT(DISTINCT ft.from_address) AS active_addresses
-  FROM flow.core_evm.fact_transactions ft
-  CROSS JOIN time_periods tp
-  WHERE ft.block_timestamp >= tp.start_date
+    SELECT CASE '{{Period}}'
+        WHEN 'all_time' THEN '2020-01-01'::DATE
+        WHEN 'last_year' THEN CURRENT_DATE - INTERVAL '1 YEAR'
+        WHEN 'last_3_months' THEN CURRENT_DATE - INTERVAL '3 MONTHS'
+        WHEN 'last_month' THEN CURRENT_DATE - INTERVAL '1 MONTH'
+        WHEN 'last_week' THEN CURRENT_DATE - INTERVAL '1 WEEK'
+        ELSE CURRENT_DATE - INTERVAL '1 DAY'
+    END AS start_date, CURRENT_DATE AS end_date
 )
-SELECT SUM(active_addresses) AS total_active_addresses
-FROM active_addresses;
+SELECT COUNT(DISTINCT from_address) AS total_active_addresses
+FROM flow.core.ez_transaction_actors ft
+       LATERAL FLATTEN(INPUT => b.actors) a
+CROSS JOIN time_periods tp
+WHERE ft.block_timestamp >= tp.start_date
+  AND ft.block_timestamp <  tp.end_date
 """
 
 SQL_ACTIVE_ACCOUNTS_EVM = """
